28a26f37c9ae51cf19963f0b21c95f5df4d415c5
camilaleutz <leutz1989@gmail.com>
2023-03-10 19:21:40 -0300

    digitando no input e pegando valores botão search funcionando em sincronia

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-//01078643f6c0de146bda7acdebfc6cf9
+const apiKey = "01078643f6c0de146bda7acdebfc6cf9";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+
+const showWeatherData = (city) => { 
+    console.log(city)
+
+
+}
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city)
+
+});
