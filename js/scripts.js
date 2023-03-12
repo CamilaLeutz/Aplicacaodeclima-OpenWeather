9594e30f8da1ddeedcee36bbc061fd9c93c5e0bf
camilaleutz <leutz1989@gmail.com>
2023-03-12 11:48:40 -0300

    aplicação funcionando, inclusive a imagem tbm depois da troca da api

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,45 @@
 const apiKey = "01078643f6c0de146bda7acdebfc6cf9";
-const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiCountryURL = "https://www.countryflagicons.com/FLAT/64/";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const humidityElement = document.querySelector("#humidity span")
+const windElement = document.querySelector("#wind span")
 
-const showWeatherData = (city) => { 
-    console.log(city)
+const weatherContainer = document.querySelector("#weather-data")
 
+const getWeatherData = async (city) => {
 
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    return data
+
+}
+
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city)
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+      );
+      
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country + ".png");
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide")
 }
 
 searchBtn.addEventListener("click", (e) => {
@@ -17,5 +48,13 @@ searchBtn.addEventListener("click", (e) => {
     const city = cityInput.value;
 
     showWeatherData(city)
-
 });
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter"){
+        const city = e.target.value
+
+        showWeatherData(city)
+    }
+})
+
